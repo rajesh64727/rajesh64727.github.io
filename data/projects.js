9604ce5184fcd8bb90ce2f9bb7999ece400272da
RajeshKumar <rajesh64727@gmail.com>
2020-02-28 15:00:47 +0530

    hide the WIP modules

---
+++ data/projects.js
@@ -0,0 +1,23 @@
+const project = {
+id:'',
+name:'',
+is_freelance:false,
+client:'',
+description:'',
+technologies:[],
+url:'',
+start_date:'',
+end_date:'',
+};
+
+const company = {
+    id:'',
+    name:'',
+    location:[],
+    designation:'',
+    skills:'',
+    profile:'',
+    achievements:[],
+    status:''
+};
+
